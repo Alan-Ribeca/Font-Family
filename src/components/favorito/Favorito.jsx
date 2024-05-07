4b13b8eff0ca2af6c55bfef37ae81d1b115477a6
Alan Ribeca <alanribeca2003@gmail.com>
2024-05-07 08:38:53 -0300

    creando la seccion mi favorito

--- src/components/favorito/Favorito.jsx
+++ src/components/favorito/Favorito.jsx
@@ -5,13 +5,24 @@ export const Favorito = () => {
 
   return (
     <>
-      <section>
-        <h1>Mis Favoritos</h1>
+      <section className="misFavoritos">
+        <h1 className="title">Mis Favoritos</h1>
 
-        <div className="ContainerFavoritos">
+        <div className="containerFavoritos">
           {datosTraidos.map((nombre, index) => (
-            <div key={index} className="favoritos">
-              <p>El nombre es {nombre}</p>
+            <div key={index} className="cuadradoFav">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="estrella"
+                viewBox="0 0 16 16"
+              >
+                <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+              </svg>
+              <p className="name">Fuente: {nombre}</p>
+              <button className="btn">Ver fuente</button>
             </div>
           ))}
         </div>
